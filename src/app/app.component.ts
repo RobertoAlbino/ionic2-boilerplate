14f1f2fdaae8189cece1b05b8670209b5a6f1f74
Roberto Albino <junior_wash@hotmail.com>
2018-06-30 15:49:55 -0300

    Alterando a splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -29,16 +29,12 @@ export class MyApp {
 
   initializeApp() {
     this.platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();
       Splashscreen.hide();
     });
   }
 
   openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
     this.nav.setRoot(page.component);
   }
 }
